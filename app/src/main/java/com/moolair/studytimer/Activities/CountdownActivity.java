42802e996ae3b4458046506ef82932d176a6dfd4
youngjinjo <moolairdev@gmail.com>
2020-05-09 21:39:36 -0700

    update: - added pause image. - calcalate total time (think about remove the time so it needs to subtract the time) - countdown the timer todo: display the time in a timeStart activity. //Todo: also, once it finishes, it should  and move on to rest calculation --> then grab the next time and calculate so and so forth.
    Todo:
    1. add rest time on main page.
    2. countdown hour and minute.
    3. move on the next screen.
    4. Add Ads.
    5. swipe to delete or undo

---
+++ app/src/main/java/com/moolair/studytimer/Activities/CountdownActivity.java
@@ -0,0 +1,14 @@
+package com.moolair.studytimer.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CountdownActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_countdown);
+    }
+}
