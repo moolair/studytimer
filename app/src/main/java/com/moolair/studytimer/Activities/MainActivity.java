2824b34ae4f6c1a54225b7c7fc8adcb7074d3c70
YoungJin Jo <yjo8181@gmail.com>
2020-08-19 22:27:32 -0700

    Trying to use Facebook Ads but it's not working
    todo:
    -during timer running, it should play background on the phone.
    
    bug: swap position doesn't get recognized when the app is completely closed and re-opened. This should be related to db update not recognized. Also, it should be similar fix method when adding items it doesn't dismiss but startActivity to view the addition.
    
    Todo (in the future):
    7. Menu
      7.1 Theme color light change (light and dark in the setting page)
      7.2 Delete all db
      7.3 purchase an app
    9. when going back button is clicked, progress bar show how much is completed and pause the time.
      9.1 (when go back to main page) Pause and resume might require requestCode so that it can stop the time with what's left. (putextra? nextIntent also might need to be required.) During time running, they cannot re-order items. or can they? I believe with linkedlist it can. FYI, it is using just array.

--- app/src/main/java/com/moolair/studytimer/Activities/MainActivity.java
+++ app/src/main/java/com/moolair/studytimer/Activities/MainActivity.java
@@ -1,9 +1,9 @@
 package com.moolair.studytimer.Activities;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
+//import android.content.SharedPreferences;
+//import android.graphics.Color;
+//import android.graphics.drawable.ColorDrawable;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.SoundPool;
@@ -17,6 +17,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,23 +27,29 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.solver.Cache;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-import com.google.android.gms.ads.AdListener;
+//import com.google.android.gms.ads.AdRequest;
+//import com.google.android.gms.ads.InterstitialAd;
+//import com.facebook.ads.InterstitialAd;
 import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.material.snackbar.Snackbar;
 import com.moolair.studytimer.Data.DBHandler;
 import com.moolair.studytimer.Model.Timer;
 import com.moolair.studytimer.R;
 import com.moolair.studytimer.UI.RecyclerViewAdapter;
 
+import java.security.AllPermission;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
+//Ad partner network
+import com.facebook.ads.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -86,18 +93,29 @@ public class MainActivity extends AppCompatActivity {
 //    private CountDownTimer countDownTimer;
 //    private long totalTime;
 
-    //Interstitial Admob
+    //Interstitial Facebook Ad
     private InterstitialAd interstitialAd;
+    private final String TAG = MainActivity.class.getSimpleName();
+    public static boolean isAdLoadedOnce = false;
 
     //Sounds
     private SoundPool soundPool;
     private int startSound, stopSound;
 
+    //AdView
+    private AdView adView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        // Initialize the Audience Network SDK
+        AudienceNetworkAds.initialize(this);
+
 
         /*YJ - April 4, 2020
         logo and ad page come here.
@@ -151,25 +169,108 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //interstitial Admob
-        interstitialAd = new InterstitialAd(this);
-        interstitialAd.setAdUnitId("ca-app-pub-8927512082902017/5298020641");
-//        interstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712"); for testing
-        interstitialAd.loadAd(new AdRequest.Builder().build());
+//        interstitialAd = new InterstitialAd(this);
+//        interstitialAd.setAdUnitId("323685912162312_323688692162034");
+////        interstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712"); for testing
+//        interstitialAd.loadAd(new AdRequest.Builder().build());
+//
+//        interstitialAd.setAdListener(new AdListener(){
+//            @Override
+//            public void onAdClosed(){
+//                interstitialAd.loadAd(new AdRequest.Builder().build());
+//                if (nextIntent != listItems.size()-1) {
+//                    restIntent();
+//                } else {
+//                    interstitialAds();
+//                    nextIntent = 0;
+//                    //todo: try to erase all the db timelist
+//                    //todo: clear buffer? memoryleak clear?
+//                }
+//            }
+//        });
+        //-----------AdMob Interstital end----------------
 
-        interstitialAd.setAdListener(new AdListener(){
-            @Override
-            public void onAdClosed(){
-                interstitialAd.loadAd(new AdRequest.Builder().build());
-                if (nextIntent != listItems.size()-1) {
-                    restIntent();
-                } else {
-                    interstitialAds();
-                    nextIntent = 0;
-                    //todo: try to erase all the db timelist
-                    //todo: clear buffer? memoryleak clear?
-                }
-            }
-        });
+        //--------------Interstitial Facebook Ad------------------------
+        //interstitialAd = new InterstitialAd(this, "323685912162312_323688692162034");
+        //For testing
+        interstitialAd = new InterstitialAd(this, "YOUR_PLACEMENT_ID");
+
+        interstitialAd.loadAd(interstitialAd.buildLoadAdConfig().withAdListener(new InterstitialAdListener() {
+        }));
+//        interstitialAd.loadAd(interstitialAd.buildLoadAdConfig()
+//                .withAdListener(new InterstitialAdExtendedListener() {
+//            @Override
+//            public void onRewardedAdCompleted() {
+//
+//            }
+//
+//            @Override
+//            public void onRewardedAdServerSucceeded() {
+//
+//            }
+//
+//            @Override
+//            public void onRewardedAdServerFailed() {
+//
+//            }
+//
+//            @Override
+//            public void onInterstitialActivityDestroyed() {
+//                if (nextIntent != listItems.size()-1) {
+//                    restIntent();
+//                } else {
+//                    interstitialAds();
+//                    nextIntent = 0;
+//                    //todo: try to erase all the db timelist
+//                    //todo: clear buffer? memoryleak clear?
+//                }
+//            }
+//
+//            @Override
+//            public void onInterstitialDisplayed(Ad ad) {
+//                isAdLoadedOnce = true;
+//                // Interstitial ad displayed callback
+//                Log.e(TAG, "Interstitial ad displayed.");
+//            }
+//
+//            @Override
+//            public void onInterstitialDismissed(Ad ad) {
+//                // Interstitial dismissed callback
+//                Log.e(TAG, "Interstitial ad dismissed.");
+//            }
+//
+//            @Override
+//            public void onError(Ad ad, AdError adError) {
+//                // Ad error callback
+//                Log.e(TAG, "Interstitial ad failed to load: " + adError.getErrorMessage());
+//            }
+//
+//            @Override
+//            public void onAdLoaded(Ad ad) {
+//                // Interstitial ad is loaded and ready to be displayed
+//                Log.d(TAG, "Interstitial ad is loaded and ready to be displayed!");
+//                // Show the ad
+////                interstitialAd.show();
+//
+//            }
+//
+//            @Override
+//            public void onAdClicked(Ad ad) {
+//                // Ad clicked callback
+//                Log.d(TAG, "Interstitial ad clicked!");
+//            }
+//
+//            @Override
+//            public void onLoggingImpression(Ad ad) {
+//                // Ad impression logged callback
+//                Log.d(TAG, "Interstitial ad impression logged!");
+//            }
+//        }).build());
+
+
+        // For auto play video ads, it's recommended to load the ad
+        // at least 30 seconds before it is shown
+        interstitialAd.loadAd();
 
         timerList = new ArrayList<>();
         listItems = new ArrayList<>();
@@ -221,6 +322,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onDestroy() {
+        if (interstitialAd != null) {
+            interstitialAd.destroy();
+        }
+        super.onDestroy();
+    }
+
     private void restIntent() {
         Intent restIntent = new Intent(MainActivity.this, CountdownActivity.class);
         restIntent.putExtra("subject", restSubject.getText().toString());
@@ -244,12 +353,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void interstitialAds() {
-        //before going to rest, let's interstitial Admob
-        if (interstitialAd.isLoaded()) {
+//        before going to rest, let's interstitial Facebook Ad
+        if (interstitialAd.isAdLoaded()) {
             interstitialAd.show();
         } else {
             Log.d("TAG", "The interstitial wasn't loaded yet.");
         }
+//        new Handler().postDelayed(new Runnable() {
+//            public void run() {
+//                // Check if interstitialAd has been loaded successfully
+//                if(interstitialAd == null || !interstitialAd.isAdLoaded()) {
+//                    return;
+//                }
+//                // Check if ad is already expired or invalidated, and do not show ad if that is the case. You will not get paid to show an invalidated ad.
+//                if(interstitialAd.isAdInvalidated()) {
+//                    return;
+//                }
+//                // Show the ad
+//                interstitialAd.show();
+//            }
+//        }, 1
+//                //1000 * 60 * 15
+//        ); // Show the ad after 15 minutes
     }
 
     ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(
